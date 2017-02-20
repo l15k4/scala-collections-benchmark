f88e5c10f74e19aba77e206e7708c7567915d181
l15k4 <liska.jakub@gmail.com>
2017-02-20 16:55:44 +0000

    bug fix, use mutable version of append for Buffers

--- src/test/scala/AppendBench.scala
+++ src/test/scala/AppendBench.scala
@@ -4,15 +4,17 @@ import org.scalameter.{Bench => _, Executor => _}
 
 import scala.collection.immutable.Range.Inclusive
 import scala.collection.immutable.{IndexedSeq, List, Vector}
+import scala.collection.mutable
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 
 trait AppendBench extends CollectionBenchmarkSupport {
   def appendSeq(empty: Seq[Int], c: IndexedSeq[Int]) = c.foldLeft(empty) { case (acc, e) => acc :+ e }
+  def appendBuffer(empty: mutable.Buffer[Int], c: IndexedSeq[Int]) = c.foldLeft(empty) { case (acc, e) => acc += e }
 
   def appendList        (c: IndexedSeq[Int]) = appendSeq(List.empty[Int], c)
   def appendVector      (c: IndexedSeq[Int]) = appendSeq(Vector.empty[Int], c)
-  def appendListBuffer  (c: IndexedSeq[Int]) = appendSeq(ListBuffer.empty[Int], c)
-  def appendArrayBuffer (c: IndexedSeq[Int]) = appendSeq(ArrayBuffer.empty[Int], c)
+  def appendListBuffer  (c: IndexedSeq[Int]) = appendBuffer(ListBuffer.empty[Int], c)
+  def appendArrayBuffer (c: IndexedSeq[Int]) = appendBuffer(ArrayBuffer.empty[Int], c)
   def appendArray       (c: IndexedSeq[Int]) = c.foldLeft(Array.empty[Int]) { case (acc, e) => acc :+ e }
 
   private def bench(gen: Gen[Inclusive]) = {
